dcebd20b13a2790298e40b07bedb1125c492ce34
Anjali Hingu <anjalihingu28@gmail.com>
2023-04-08 11:37:48 +0530

    .

---
+++ WebApi/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Model;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        public static List<Category> category = new List<Category>()
+        {
+            new Category() { Id = 1,Title="Electronic"},
+            new Category() { Id = 2,Title="Fashion"},
+            new Category() { Id = 3,Title="Decoration"},
+            new Category() { Id = 4,Title="Households"}
+        };
+
+        [HttpGet]
+         public List<Category> Get() 
+         {
+            return category;
+         }
+        [HttpPost]
+
+        public IActionResult Post([FromBody]Category cat) 
+        {
+            category.Add(cat);
+            return Ok(category);
+        }
+
+    }
+}
---
+++ WebApi/Startup.cs
@@ -0,0 +1,39 @@
+﻿public class Startup
+{
+    public IConfiguration configRoot
+    {
+        get;
+    }
+    public Startup(IConfiguration configuration)
+    {
+        configRoot = configuration;
+    }
+    readonly string _specificOrigin = "specificOrigin";
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddCors(o=>
+        {
+            o.AddPolicy("_specificOrigin",
+                p=>p.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+        });
+        services.AddRazorPages();
+    }
+    public void Configure(WebApplication app, IWebHostEnvironment env)
+    {
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+        app.UseCors(_specificOrigin);
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.MapRazorPages();
+        app.Run();
+    }
+}
